ccf6efb4161af9012e25ad6f309e598b21f06e5f
diddy48uni <gabriele2.romano@mail.polimi.it>
2024-09-13 16:58:36 +0200

    added camera server and thingspeak version

---
+++ SmartPlantHardware/plant_sensor_thingspeak/Keys.h
@@ -0,0 +1,6 @@
+
+#define PLANTBOOK_API_KEY "12fb8eb45d63fd1bba9518ffaac0017a0a15484e"
+
+#define SECRET_CH_ID 2655187			// replace 0000000 with your channel number
+#define SECRET_WRITE_APIKEY "QXHWUHZPDXK7JWQR"   // replace XYZ with your channel write API Key
+
